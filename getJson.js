2596ff4f743945ab55669a1d07ef2097c14181d2
rebeks314 <rebecka.ekstrom@hotmail.se>
2015-02-26 11:06:41 +0100

    slut på lektion

--- getJson.js
+++ getJson.js
@@ -2,11 +2,35 @@ console.log("work");
 
 $(document).ready(function() {
 
-    "getjson.json",
-            function(data) {
+    $.getJSON('getJson.json',function(data) {
 
                 $("#div1").text(data.lista);
+                
+                $.each(data.lista, function(key, val){
+                    $('<li id"' + '">').text(val).appendTo("#div1");
+              
+               
+                });
 
-            };
+            });
+            
+            $('.grupp').change(function(data) {
+                
+                $('.subgrupp option').remove();
+                
+                switch (data){
+                    
+                    case 0:
+                    case 1:   
+                        
+                
+                
+                $('.subgrupp').append('<option value="0">Liten</option>');
+                $('.subgrupp').append('<option value="0">Stor</option>');
+                console.log("lista");
+        
+                }
+                
+            });
 
 });
--- index.php
+++ index.php
@@ -10,7 +10,7 @@ and open the template in the editor.
         <title>Hejsan!</title>
         
 <!--        <script src="getJosn.json"></script>--> 
-        <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+       <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
         <script src="getJson.js"></script>
        
         
@@ -18,6 +18,53 @@ and open the template in the editor.
     <body>
         
         <div id="div1"></div>
+        <p class="mule"></p>
+        
+<!--         <button class="btn">Button!</button>
+        <button class="read">Läs in JSON</button>-->
+        
+        
+         <select name="grupp" class="grupp">
+            <option value="">djur</option>
+            <option value="0">katt</option>
+            <option value="1">hund</option>
+        </select>
+        <select name="subgrupp" class="subgrupp">
+
+        </select>
+        <p class="json-target"></p>
+         
+        
+         
+<!--         <script>
+//            console.log("Jacob");
+            document.getElementById(".mule").innerHTML = "ny text";
+            $('#div1').text("ny text igen");
+            $('.btn').click(function() {
+                //$('.btn').hide();
+                $('.btn').animate(
+                        {width: 500, height: 700, left: 1000}, 5000);
+                console.log("klick körs. Knapp gömd.");
+            });
+            $('.read').click(function() {
+                console.log("read klickad");
+                $.getJSON("getJson.json", function(data) {
+                    console.log("getJSON körs");
+                    console.log(data);
+                    $('.mule').append("<p>" + data.namn + "</p>");
+                    $('.mule').append("<p>" + data.ålder + "</p>");
+                });
+            });
+            $('.grupp').change(function() {
+                console.log("change triggad");
+                console.log($('.grupp').val());
+                $.getJSON("getJson.json", function(data) {
+                    console.log(data);
+                    $('.subgrupp').remove();
+                    $('.subgrupp').append('<option value="">gg</option>');
+                });
+            });
+        </script>-->
         
         <?php
         // put your code here
